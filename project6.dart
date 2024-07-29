b1d6dfa797eda20221ab0a445a7dfb61d268b8bd
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-07-29 16:06:59 +0500

    sixt project

--- project6.dart
+++ project6.dart
@@ -1,7 +1,34 @@
+import 'dart:html_common';
+import 'dart:math';
+
 void main() {
-  List usersEligibility = ['John','Alice','eligible''Mike','Sarah','Tom'];
+  List<int> numbers = [89, 56, 85, 45, 1, 66];
+  int small = numbers.reduce(min);
+  int great = numbers.reduce(max);
+
+  print('Smallest number: $small');
+  print('Greatest number: $great');
+
+  List<String> usersEligibility = [
+    'John',
+    'Alice',
+    'eligible',
+    'Mike',
+    'Sarah',
+    'Tom'
+  ];
 
   usersEligibility.removeWhere((user) => user == 'eligible');
 
   print(usersEligibility);
+
+  List<int> numbers1 = [67, 89, 34, 88, 54, 87];
+
+  int maxValue = numbers1.reduce(max);
+  print('Maximum value: $maxValue');
+}
+
+  var vals = ["89,56,76,59,86,57,69,70"];
+  var reversed = List.of(vals.reversed);
+  print(reversed);
 }
