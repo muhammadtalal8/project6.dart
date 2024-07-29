cb5071f75f5dacea031dac57f24b76d56b012583
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-07-29 15:01:33 +0500

    5 assignment

--- project4.dart
+++ project4.dart
@@ -1,6 +1,9 @@
 void main() {
-  print("hello world");
-  String name = "talal";
+  List name = ["muhamamd,bilal,talal,anas,ali"];
   print(name);
+  List days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+  print(days[6]);
 
+  List student =[ "talal","10 grade","23","72"];
+  print(student);
 }
